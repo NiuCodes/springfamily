259cb05879d87b3f530791df99dc44c7104949f3
Casey <814704069@qq.com>
2020-09-28 22:54:34 +0800

    spring cloud ribbon support

---
+++ springcloud-ribbon/src/main/java/com/nkx/springcloudribbon/SpringcloudRibbonApplication.java
@@ -0,0 +1,13 @@
+package com.nkx.springcloudribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudRibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudRibbonApplication.class, args);
+    }
+
+}
---
+++ springcloud-ribbon/src/test/java/com/nkx/springcloudribbon/SpringcloudRibbonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nkx.springcloudribbon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudRibbonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
