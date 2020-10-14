4c3f41e5ecc2d8b422076a8bf643249a62f94d83
C5310928 <kaixi.niu@sap.com>
2020-10-14 12:23:29 +0200

    springcloud zuul support

--- springcloud-order/src/main/java/com/nkx/springcloudorder/SpringcloudOrderApplication.java
+++ springcloud-order/src/main/java/com/nkx/springcloudorder/SpringcloudOrderApplication.java
@@ -2,15 +2,12 @@ package com.nkx.springcloudorder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
-@EnableCircuitBreaker
 public class SpringcloudOrderApplication {
 
     public static void main(String[] args) {
